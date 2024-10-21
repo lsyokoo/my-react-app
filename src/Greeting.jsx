57161caf5f9f5ee1754310977d609d7efbe63236
lsyokoo <lsy20041203@gmail.com>
2024-10-21 17:51:30 -0400

    first commit

---
+++ src/Greeting.jsx
@@ -0,0 +1,8 @@
+// Welcome.jsx
+import React from 'react';
+
+function Welcome(props) {
+  return <h1>hello, {props.name}</h1>;
+}
+
+export default Welcome;
---
+++ src/StyledButton.jsx
@@ -0,0 +1,9 @@
+function StyledButton() {
+    return (
+      <button className="myBtn" style={{ backgroundColor: 'blue', color: 'white', padding: '10px 20px' }}>
+        Click Me
+      </button>
+    );
+  }
+
+  export default StyledButton
